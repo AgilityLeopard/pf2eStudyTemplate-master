9615f09c833643bba9b22f293dec146b72a8cea5
AgilityLeopard <agilityleopard@gmail.com>
2025-01-24 11:35:07 +0300

    1111

--- serverMiddleware/forcedomain.js
+++ serverMiddleware/forcedomain.js
@@ -2,9 +2,9 @@ export default function (req, res, next) {
   const host = req.headers.host
   const url = req.url
   const env = process.env.NODE_ENV
-  const forceDomain = 'https://pf2e-builder-ru.netlify.app'
+  const forceDomain = 'http://localhost:3000'
 
-  if (env === 'production' && host === 'pf2e-builder-ru.netlify.app') {
+  if (env === 'production' && host === 'localhost:3000') {
     res.writeHead(301, { Location: forceDomain + url })
     return res.end()
   }
