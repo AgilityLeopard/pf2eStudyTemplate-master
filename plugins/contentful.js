60afacac932ea7eeeac60d2bd1bbe65ca241a57b
AgilityLeopard <agilityleopard@gmail.com>
2026-04-16 15:53:50 +0300

    111

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,14 +1,10 @@
-const contentful = require('contentful');
-// use default environment config for convenience
-// these will be set via `env` property in nuxt.config.js
-const config = {
-  space: process.env.NUXT_ENV_CTF_SPACE_ID,
-  accessToken: process.env.NUXT_ENV_CTF_CD_ACCESS_TOKEN,
-};
+import { createClient } from 'contentful'
 
-// export `createClient` to use it in page components
-module.exports = {
-  createClient() {
-    return contentful.createClient(config);
-  },
-};
+export default (context, inject) => {
+  const client = createClient({
+    space: process.env.NUXT_ENV_CTF_SPACE_ID,
+    accessToken: process.env.NUXT_ENV_CTF_CD_ACCESS_TOKEN,
+  })
+
+  inject('cf', client)
+}
