ce14a83d680271a5982dc1348722910fc9487d41
AgilityLeopard <agilityleopard@gmail.com>
2026-04-16 14:17:26 +0300

    11

--- nuxt.config.js
+++ nuxt.config.js
@@ -273,7 +273,7 @@ module.exports = {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'http://127.0.0.1:3000', // ВАЖНО
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:3000',
     browserBaseURL: '/'
   },
 
