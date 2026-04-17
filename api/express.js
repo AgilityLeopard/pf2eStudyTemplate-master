c860e96363b11552349fa1b783b3e9fa94d57e36
AgilityLeopard <agilityleopard@gmail.com>
2026-04-17 08:28:09 +0300

    111

--- api/express.js
+++ api/express.js
@@ -1,13 +1,10 @@
-// https://www.nexmo.com/blog/2018/09/11/add-2fa-to-nuxt-with-nexmo-verify-dr?sf92467419=1
-// https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
-
-/**/
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 
 const mountRoutes = require('./routes');
+
 app.use((req, res, next) => {
   console.log("API HIT:", req.url);
   next();
@@ -15,9 +12,13 @@ app.use((req, res, next) => {
 
 mountRoutes(app);
 
+// 👇 ВОТ ЭТО ДОБАВЬ (самое важное)
+app.use((req, res, next) => {
+  // если Express не обработал запрос — отдай его обратно Nuxt
+  next();
+});
 
 module.exports = {
   path: '/api',
   handler: app,
 };
-
